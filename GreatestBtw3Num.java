5f658258f5f55311fc63087111f3839e9a055f1d
Shreyam Singhal <shreyamsinghal.student@gmail.com>
2025-02-28 01:03:45 +0530

    .

--- GreatestBtw3Num.java
+++ GreatestBtw3Num.java
@@ -27,3 +27,36 @@ public class GreatestBtw3Num {
         sc.close();
     }
 }
+
+
+
+// Better and easy code
+/*
+ * import java.util.Scanner;
+
+public class GreatestBtw3Num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st No : ");
+        int a = sc.nextInt();
+        System.out.print("Enter 2nd No : ");
+        int b = sc.nextInt();
+        System.out.print("Enter 3rd No : ");
+        int c = sc.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println("Greater No is " + a);
+        }
+        else if (b > a && b > c) {
+            System.out.println("Greater No is " + b);
+        }
+        else{
+            System.out.println("Greater No is " + c);
+        }
+
+        sc.close();
+    }
+}
+
+ */
